5c8fd8ae2707c4dfe561d20f882ad04a7a5550a8
VagTsop <vatsop52@gmail.com>
2021-03-04 13:42:04 +0200

    Add SumDigit challenge

---
+++ src/SumDigitChallenge.java
@@ -0,0 +1,26 @@
+public class SumDigitChallenge {
+
+    public static void main(String args[]) {
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(1));
+        System.out.println(sumDigits(8));
+    }
+
+
+    public static int sumDigits(int number) {
+
+        if (number >= 10) {
+            int sum = 0;
+            while (number > 0) {
+                // extract the least-significant digit
+                int digit = number % 10;
+                sum += digit;
+
+                // drop the least-significant digit
+                number /= 10;
+            }
+            return sum;
+        }
+        return -1;
+    }
+}
