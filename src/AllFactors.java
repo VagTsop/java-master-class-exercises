94d2138481d07d3afdf4ce198208c3bfc9aaa64f
VagTsop <vatsop52@gmail.com>
2021-03-06 19:05:53 +0200

    format file

--- src/AllFactors.java
+++ src/AllFactors.java
@@ -4,13 +4,10 @@ public class AllFactors {
     public static void main(String args[]) {
 
         printFactors(6);   // should print 1 2 3 6
-//        printFactors(32);  // should print 1 2 4 8 16 32
-//        printFactors(10);  // should print 1 2 5 10
-//        printFactors(-1);  // should print "Invalid Value" since number is <1
-
-
+        printFactors(32);  // should print 1 2 4 8 16 32
+        printFactors(10);  // should print 1 2 5 10
+        printFactors(-1);  // should print "Invalid Value" since number is <1
     }
-
 
     public static void printFactors(int number) {
         int factor = 1;
