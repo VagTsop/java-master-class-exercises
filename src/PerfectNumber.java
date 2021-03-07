3780e11749cb05f54889ab28b8132be3f1e9b1eb
VagTsop <vatsop52@gmail.com>
2021-03-07 13:46:43 +0200

    Add PerfectNumber challenge

---
+++ src/PerfectNumber.java
@@ -0,0 +1,30 @@
+public class PerfectNumber {
+
+
+    public static void main(String args[]) {
+
+        System.out.println(isPerfectNumber(6));// should return true since its proper divisors are 1 2 3  and the sum is 1 +2 +3 =6
+        System.out.println(isPerfectNumber(28));// should return true since its proper divisors are 1 2 4 7 14  and the sum is 1 + 2 + 4 + 7 + 14 = 28
+        System.out.println(isPerfectNumber(5));// should return false since its proper divisors is 1 and the sum is 1 not 5
+        System.out.println(isPerfectNumber(-1));// should return false since the number is <1
+
+    }
+
+    public static boolean isPerfectNumber(int number) {
+
+        int perfect = 1;
+        int perfectNumber = 0;
+        if (number > 1) {
+            while (perfect < number) {
+                if (number % perfect == 0) {
+                    perfectNumber += perfect;
+                }
+                perfect++;
+            }
+            if (perfectNumber == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
