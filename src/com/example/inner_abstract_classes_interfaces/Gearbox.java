0ad5aee82136881c8c353bed33c25a71fd71fb25
VagTsop <vatsop52@gmail.com>
2021-03-19 18:43:50 +0200

    add interface project

---
+++ src/com/example/inner_abstract_classes_interfaces/Gearbox.java
@@ -0,0 +1,10 @@
+package com.example.inner_abstract_classes_interfaces;
+
+public class Gearbox {
+
+    private boolean clutchIsIn;
+
+    public void operateClutch(boolean inOrOut) {
+        this.clutchIsIn = inOrOut;
+    }
+}
---
+++ src/com/example/inner_abstract_classes_interfaces/ITelephone.java
@@ -0,0 +1,16 @@
+package com.example.inner_abstract_classes_interfaces;
+
+
+public interface ITelephone {
+    void powerOn();
+
+    void dial(int phoneNumber);
+
+    void answer();
+
+    boolean callPhone(int phoneNumber);
+
+    boolean isRinging();
+}
+
+
