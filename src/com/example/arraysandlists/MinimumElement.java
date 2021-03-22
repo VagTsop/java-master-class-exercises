7a04324f2635d9d100f0a4fce0809d0d4f8a6c5c
VagTsop <vatsop52@gmail.com>
2021-03-22 17:46:51 +0200

    add minimumElement challenge

---
+++ src/com/example/arraysandlists/MinimumElement.java
@@ -0,0 +1,43 @@
+package com.example.arraysandlists;
+
+
+import java.util.Scanner;
+
+public class MinimumElement {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static Scanner scanner;
+
+    private static int readInteger() {
+        scanner = new Scanner(System.in);
+        int capacity = scanner.nextInt();
+        scanner.close();
+        return capacity;
+    }
+
+    private static int[] readElements(int values) {
+        scanner = new Scanner(System.in);
+        int[] elements = new int[values];
+        for (int i = 0; i < elements.length; i++) {
+            int totalElementEntered = scanner.nextInt();
+            scanner.nextLine();
+            elements[i] = totalElementEntered;
+
+        }
+        return elements;
+    }
+
+    private static int findMin(int[] count) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < count.length; i++) {
+            int minVal = count[i];
+            if (minVal < min) {
+                min = minVal;
+            }
+        }
+        return min;
+    }
+}
